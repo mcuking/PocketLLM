bc8860b9b49852f927218781ef0fe4a93e35c87e
mcuking <tangjianghong@163.com>
2025-06-30 18:50:45 +0800

    feat: 实现可训练权重的自注意力机制：计算所有输入词元的上下文向量

--- attention.py
+++ attention.py
@@ -9,7 +9,6 @@ inputs = torch.tensor(
    [0.05, 0.80, 0.55]] # step     (x^6)
 )
 
-x_2 = inputs[1]
 d_in = inputs.shape[1]
 d_out = 2
 
@@ -28,27 +27,27 @@ W_value = torch.nn.Parameter(torch.randn(d_in, d_out), requires_grad=False)
 print("W_query:", W_query)
 
 # 计算查询、键和值向量
-query_2 = x_2 @ W_query
+queries = inputs @ W_query
 keys = inputs @ W_key
 values = inputs @ W_value
-print("query_2:", query_2)
+print("queries:", queries)
 print("keys:", keys)
 print("values:", values)
 
 # 计算注意力分数
-# 通过计算 query_2 和每个键向量的点积和
-attention_scores_2 = query_2 @ keys.T
-print("attention_scores_2:", attention_scores_2)
+# 通过计算每个查询向量和每个键向量的点积来计算注意力分数
+attention_scores = queries @ keys.T
+print("attention_scores:", attention_scores)
 
 # 计算注意力权重
 # 使用 softmax 函数将注意力分数转换为权重
 # 注意力分数除以键向量的维度的平方根，以防止梯度消失
 # 这里的 d_k 是键向量的维度
 d_k = keys.shape[-1]  # 键向量的维度
-attention_weights_2 = torch.softmax(attention_scores_2 / d_k**0.5, dim=-1)
-print("attention_weights:", attention_weights_2)
+attention_weights = torch.softmax(attention_scores / d_k**0.5, dim=-1)
+print("attention_weights:", attention_weights)
 
 # 计算上下文向量
 # 上下文向量是注意力权重和值向量的加权和
-context_vector_2 = attention_weights_2 @ values
-print("context_vector_2:", context_vector_2)
+context_vector = attention_weights @ values
+print("context_vector:", context_vector)
