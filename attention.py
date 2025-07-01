7d9c00c406da9fce36f20c820b83d8002a72a2db
mcuking <tangjianghong@163.com>
2025-07-01 14:17:01 +0800

    feat: 实现可训练权重的自注意力机制：使用 nn.Linear 层优化权重矩阵初始化方案

--- attention.py
+++ attention.py
@@ -11,7 +11,7 @@ inputs = torch.tensor(
 
 # 当我们定义一个PyTorch模型时，通常会继承`torch.nn.Module`类
 class SelfAttention_v1(torch.nn.Module):
-    def __init__(self, d_in, d_out):
+    def __init__(self, d_in, d_out, qkv_bias=False):
         super().__init__()
         # 生成三个权重矩阵：
         # 查询矩阵Wq：表示“我需要什么信息“
@@ -21,18 +21,18 @@ class SelfAttention_v1(torch.nn.Module):
         # 查询空间：让“sat”的查询向量能够询问“谁在坐？”（关注主语）和“坐在哪里？”（关注地点）。
         # 键空间：让“cat”的键向量能够表示“我是主语”，而“mat”的键向量表示“我是地点”。
         # 值空间：当“cat”被关注时，它的值向量提供关于主语的详细信息（如“cat”是动物，单数等）；当“mat”被关注时，它的值向量提供关于地点的信息（如“mat”是物体，在下面等）。
-        self.W_query = torch.nn.Parameter(torch.randn(d_in, d_out), requires_grad=False)
-        self.W_key = torch.nn.Parameter(torch.randn(d_in, d_out), requires_grad=False)
-        self.W_value = torch.nn.Parameter(torch.randn(d_in, d_out), requires_grad=False)
+        self.W_query = torch.nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = torch.nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = torch.nn.Linear(d_in, d_out, bias=qkv_bias)
 
     # 我们在子类中定义`forward`方法，该方法描述了前向传播的计算过程。
     def forward(self, x):
         # 计算查询、键和值向量
         # 通过输入 x 和权重矩阵进行矩阵乘法
         # 这里的 x 是输入的特征矩阵
-        queries = x @ self.W_query
-        keys = x @ self.W_key
-        values = x @ self.W_value
+        queries = self.W_query(x)
+        keys = self.W_key(x)
+        values = self.W_value(x)
 
         # 计算注意力分数
         # 通过计算每个查询向量和每个键向量的点积来计算注意力分数
@@ -59,6 +59,7 @@ sa_v1 = SelfAttention_v1(d_in, d_out)
 # 当我们像函数一样调用模块实例（例如`sa_v1(inputs)`）时，
 # 实际上调用的是`__call__`方法， `__call__`方法内部会调用`forward`方法，
 # 同时还会处理一些其他事情（例如钩子、梯度记录等）。
-# 用户只需要定义`forward`方法，而无需关心调用细节。
+# 神经网络层本质是数学函数：y = f(x)
+# f(x) 比 f.forward(x) 更符合数学直觉
 context_vector = sa_v1(inputs)
 print("Context Vector:\n", context_vector)
